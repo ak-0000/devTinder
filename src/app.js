b8a41e4fd52469359db74286cd2da64a68c6dc1a
ak-0000 <atulbhakra@gmail.com>
2024-09-17 23:24:08 +0530

    exploring get , post ,delete

--- src/app.js
+++ src/app.js
@@ -2,19 +2,38 @@ const express = require("express");
 
 const app = express() ;
 
-// app.use("/", (req , res) =>{
-//     res.send("this is the dashboard");
-// })       this will be used for all the paths starting with / When the "/"
-//  route is matched, it sends a response, which ends the request-response cycle, 
-// so none of the other routes are being reached.
+// app.use("/user" , (req , res ) => {
+//     res.send("this will be used for all types of http api call ");
+// })
 
-app.use("/test" , (req , res) => {
-    res.send("res is send ");
+app.get("/user" , (req , res) => {x
+    res.send({Name : "Atul " ,  Age : 19});
+});
+
+app.post("/user" , (req , res ) => {
+    res.send("data is added to the database ");
 })
 
+app.delete("/user" , (req , res) => {
+    res.send("data is deleted from the database ");
+})
+
+app.patch("/user" , (req , res ) => {
+    res.send("data is updated in the database");
+})
+
+// this will match all the http method api calls to /hello 
 app.use("/hello" , (req , res) =>{
     res.send("hello theres");
-})  
+});  
+
+// app.use("/", (req , res) =>{
+//     res.send("this is the dashboard");
+// });       //this will be used for all the paths starting with / When the "/" or /hello - /hello/1 -> same route 
+//  route is matched, it sends a response, which ends the request-response cycle, 
+// so none of the other routes are being reached.
+// /hello - /hello123 -> does not match 
+
 
 app.listen(3000 , () => {
     console.log("server is listenting ");
