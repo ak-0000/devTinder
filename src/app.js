c8a4b4cb3b83df0d73abc0e015b99cbbaf7a6c72
ak-0000 <atulbhakra@gmail.com>
2024-09-21 14:43:17 +0530

    authuser and authadmin

--- src/app.js
+++ src/app.js
@@ -1,46 +1,40 @@
 const express = require("express");
+const {authadmin, authuser} = require("./middleware/auth")
+const app = express();
 
-const app = express() ;
+// get user => chain of middleware ==> request handler 
 
-// app.use("/user" , (req , res ) => {
-//     res.send("this will be used for all types of http api call ");
-// })
-app.get("/user" , (req , res) => {
-    console.log(req.query);
-    res.send({Name : "Atul " ,  Age : 19});
+// app.use("/user " , [rh1 , rh2 ], rh3 );  // maintaining multiple round handlers 
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("1 response");              //middleware 
+//     next(); 
+//     // res.send("1 is working");
+//   },
+//   (req, res, next) => {
+//       next() ; 
+//     // res.send("2 is working");
+//   } 
+//   ,
+//   (req , res , next) => {
+//     res.send("3 is working ");                 //request handler 
+//   }
+// );
+
+app.use("/admin" , authadmin);
+
+app.get("/admin/getdata" , (req, res) => {
+  res.send("there is the data ");
 });
 
+// app.use("/user" , authuser);
 
-app.get("/user/:userid" , (req , res) => {
-    console.log(req.params);
-    res.send({Name : "Atul " ,  Age : 19});
-});
-
-app.post("/user" , (req , res ) => {
-    res.send("data is added to the database ");
+app.get("/user/data", authuser , (req , res) => {
+  res.send("user name is elon musk");
 })
 
-app.delete("/user" , (req , res) => {
-    res.send("data is deleted from the database ");
-})
 
-app.patch("/user" , (req , res ) => {
-    res.send("data is updated in the database");
-})
-
-// this will match all the http method api calls to /hello 
-app.use("/hello" , (req , res) =>{
-    res.send("hello theres");
-});  
-
-// app.use("/", (req , res) =>{
-//     res.send("this is the dashboard");
-// });       //this will be used for all the paths starting with / When the "/" or /hello - /hello/1 -> same route 
-//  route is matched, it sends a response, which ends the request-response cycle, 
-// so none of the other routes are being reached.
-// /hello - /hello123 -> does not match 
-
-
-app.listen(3000 , () => {
-    console.log("server is listenting ");
+app.listen(3000, () => {
+  console.log("server is listenting ");
 });
---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+const authadmin = (req , res , next) => {
+    console.log("checking the auth");
+    const token = "zxc";
+    const checkingtoken = token === "secret";
+    if(!checkingtoken){
+        res.status(401).send("you are not a admin ");
+    }
+    else{
+        next();
+    }
+}
+
+const authuser = (req , res , next) => {
+    console.log("checking the auth");
+    const token = "secret";
+    const checkingtoken = token === "secret";
+    if(!checkingtoken){
+        res.status(401).send("you are not a user ");
+    }
+    else{
+        next();
+    }
+}
+
+
+
+module.exports = {
+    authadmin,
+    authuser,
+}
